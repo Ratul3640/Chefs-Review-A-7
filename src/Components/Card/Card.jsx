2cc93ff5a6d97929d8c53e841a8a04679b1a19f9
Nahanur-islam <nahnanurism71@gmail.com>
2024-10-24 21:04:46 +0600

    Cards section completed

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { GoDotFill } from "react-icons/go";
+import { FaRegClock } from "react-icons/fa6";
+import { SlEnergy } from "react-icons/sl";
+import PropTypes from 'prop-types';
+
+
+const Card = ({ card, handleWantToCook }) => {
+    const { recipe_name, short_description, recipe_image, preparing_time, calories, ingredients } = card;
+    return (
+        <div>
+            <div className="card  bg-base-100 shadow-xl ">
+                <figure><img src={recipe_image} alt="" /></figure>
+                <div lassName="card-body">
+                    <h2 className='text-xl mb-4 card-title text-[#282828]'>{recipe_name}</h2>
+                    <p className='mb-2 font-normal text-base text-[#878787]'>{short_description}</p> <hr />
+
+                    <div>
+                        <h1 className='text-lg font-medium mb-4'>Ingredients: {ingredients.length}</h1>
+
+
+                        <div>
+                            <p className="flex items-center text-[#878787] font-normal"><GoDotFill /> <span>{ingredients[0]}</span>
+                            </p>
+                            <p className="flex items-center text-[#878787] font-normal"><GoDotFill /><span>{ingredients[1]}</span></p>
+                            <p className="flex items-center text-[#878787] font-normal"><GoDotFill /><span>{ingredients[2]}</span></p>
+                        </div> <hr />
+                        <div className="mt-5 flex items-center justify-between">
+                            <p className="flex items-center gap-1 text-[#282828] text-opacity-70 text-base font-normal"><FaRegClock />
+                            </p>
+                            <p className="flex items-center gap-1 text-[#282828] text-opacity-70 text-base font-normal"><SlEnergy />
+                            </p>
+                        </div>
+
+                    </div>
+                    <div className='card-actions'>
+                        <button onClick={() => handleWantToCook(card)} className="btn btn-active btn-accent text-lg text-[#150B2B] font-medium rounded-3xl mt-5">Want to Cook</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+
+Card.propTypes = {
+    card: PropTypes.object,
+    handleWantToCook: PropTypes.func
+}
+export default Card;
---
+++ src/Components/Navbar/Cards/Cards.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import Card from '../../Card/Card';
+import PropTypes from 'prop-types';
+
+
+
+const Cards = () => {
+    const [Cards, setCards] = useState([]);
+
+
+    useEffect(() => {
+        fetch('card.json')
+            .then(res => res.json())
+            .then(data => setCards(data))
+    }, [])
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-3 items-center lg:w-2/3 w-full">
+
+            {
+                Cards.map(card =><Card handleWentToCook={handleWentToCook} key={card.id} card={card}></Card>)
+            }
+
+        </div>
+    );
+};
+
+
+Cards.propTypes = {
+    handleWantToCook: PropTypes.func
+}
+export default Cards;
