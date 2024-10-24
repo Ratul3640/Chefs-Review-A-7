c362a9a9ce1d2926a0ec98f9b7bf2fe72d9e1f86
Nahanur-islam <nahnanurism71@gmail.com>
2024-10-25 00:39:27 +0600

    projects updated

--- src/Components/Navbar/Banner/Banner.jsx
+++ src/Components/Navbar/Banner/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
                 <div className="hero-content text-center text-neutral-content">
                     <div className="w-full ">
                         <h1 className="mb-6 lg:text-5xl text-3xl font-bold text-white">Discover an exceptional cooking class tailored for you!</h1>
-                        <p className="mb-10 lg:text-lg text-xs">Embark on a culinary journey with our personalized cooking class. Discover new techniques and flavors as you unleash your inner chef. Join us for an unforgettable experience in the art of cooking. Book now and elevate your skills!</p>
+                        <p className="mb-10 lg:text-lg text-xs">Join our exclusive cooking class and unlock the secrets of gourmet cuisine. Whether you're a beginner or an experienced cook, this class will enhance your culinary skills and expand your knowledge of global flavors. Learn hands-on techniques from expert chefs in a fun, interactive environment. From mastering knife skills to creating restaurant-quality dishes, we’ll guide you every step of the way!</p>
                         <div className="flex flex-col md:flex-row justify-center items-center md:gap-6 gap-4">
                             <button className="btn btn-active btn-accent text-xl font-semibold rounded-full">Explore Now</button>
                             <button className="btn btn-ghost text-xl font-semibold rounded-full text-white border-2 border-neutral-300 border-opacity-60">Our Feedback</button>
